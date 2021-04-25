dec4a8452f36c1655aabf79789dac7191c5f17ba
Vasil1001 <vaskodzhakov@gmail.com>
2021-04-26 00:30:18 +0100

    u
    u

--- v3/traffix/src/jdbc/csvData.java
+++ v3/traffix/src/jdbc/csvData.java
@@ -8,9 +8,10 @@ package jdbc;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigDecimal;
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -20,7 +21,7 @@ import java.util.Iterator;
  * @author Group D
  */
 public class csvData {
-        
+
     public static void main(String[] args) {
         Connection con = connectTrafficDB.getConnection();
         Statement stmt = null;
@@ -33,29 +34,28 @@ public class csvData {
             ArrayList list = new ArrayList();
             ArrayList IdChecker = new ArrayList();
             int count = 0;
-            
+
             while ((strLine = br.readLine()) != null) {
                 list.add(strLine);
                 count++;
-            } 
-            
+            }
+
             list.remove(0);
             Iterator itr;
             for (itr = list.iterator(); itr.hasNext();) {
                 String str = itr.next().toString();
                 String[] splitSt = str.split(",");
-                
-                
-             if (!IdChecker.contains(splitSt[5])) {
-                 IdChecker.add(splitSt[5]);
+
+                if (!IdChecker.contains(splitSt[5])) {
+                    IdChecker.add(splitSt[5]);
                     int region_id = Integer.parseInt(splitSt[5]);
-                    
+
                     String region_name = splitSt[6];
-                    
+
                     String sqlString = "INSERT or IGNORE INTO Region (region_id, region_name)VALUES \n"
                             + "('" + region_id + "','" + region_name + "');";
-                    
-                    System.out.println("sqlString: "+ sqlString);
+
+                    System.out.println("sqlString: " + sqlString);
                     con.setAutoCommit(false);
                     stmt = con.createStatement();
 
@@ -63,18 +63,17 @@ public class csvData {
                     stmt.close();
                     con.commit();
 
-                    } 
-                else {
+                } else {
                     System.out.println("Duplicate record with pk :" + splitSt[5]);
                 }
-                
+
                 if (!IdChecker.contains(splitSt[7])) {
                     IdChecker.add(splitSt[7]);
                     int local_authority_id = Integer.parseInt(splitSt[7]);
                     String local_authority_name = splitSt[8];
-                //region_id = Integer.parseInt(splitSt[5]); FROM FOREIGN KEY
+                    //region_id = Integer.parseInt(splitSt[5]); FROM FOREIGN KEY
                     String sqlString = "INSERT or IGNORE INTO Local_Authority (local_authority_id, local_authority_name) VALUES (" + local_authority_id + ",'" + local_authority_name + "');";
-                    System.out.println("sqlString : "+ sqlString);
+                    System.out.println("sqlString : " + sqlString);
                     con.setAutoCommit(false);
                     stmt = con.createStatement();
 
@@ -82,18 +81,17 @@ public class csvData {
                     stmt.close();
                     con.commit();
 
-                } 
-else {
+                } else {
                     System.out.println("Duplicate record with pk :" + splitSt[7]);
                 }
-                
+
                 if (!IdChecker.contains(splitSt[9])) {
                     IdChecker.add(splitSt[9]);
                     String road_name = splitSt[9];
                     String road_type = splitSt[10];
-                //int local_authority_id = Integer.parseInt(splitSt[7]);
+                    //int local_authority_id = Integer.parseInt(splitSt[7]);
                     String sqlString = "INSERT OR IGNORE INTO Road (road_name, road_type) VALUES ('" + road_name + "','" + road_type + "');";
-                    System.out.println("sqlString : "+ sqlString);
+                    System.out.println("sqlString : " + sqlString);
                     con.setAutoCommit(false);
                     stmt = con.createStatement();
 
@@ -101,11 +99,10 @@ else {
                     stmt.close();
                     con.commit();
 
-                } 
-            else {
+                } else {
                     System.out.println("Duplicate record with pk :" + splitSt[9]);
-                }
-                
+                } 
+
 //                if (!IdChecker.contains(splitSt[0])) {
 //                    IdChecker.add(splitSt[0]);
 //                    int count_point_id = Integer.parseInt(splitSt[0]);
@@ -120,30 +117,29 @@ else {
 //                    Double d_m = Double.valueOf(splitSt[18]);
 //                    BigDecimal link_length_miles = BigDecimal.valueOf(d_m);
 //                    //road_name = splitSt[9];
-                    
+
                 if (!IdChecker.contains(splitSt[0])) {
                     IdChecker.add(splitSt[0]);
                     int count_point_id = Integer.parseInt(splitSt[0]);
                     String start_junction_road_name = splitSt[13];
-                    String end_junction_road_name = splitSt[14];                
+                    String end_junction_road_name = splitSt[14];
                     int easting = Integer.parseInt(splitSt[15]);
                     int northing = Integer.parseInt(splitSt[16]);
-                    float latitude = Float.parseFloat(splitSt[17]);
-                    float longitude = Float.parseFloat(splitSt[18]);
+                    double latitude = Double.parseDouble(splitSt[17]);
+                    double longitude = Double.parseDouble(splitSt[18]);
                     double link_length_km = Double.parseDouble(splitSt[19]);
                     double link_length_miles = Double.parseDouble(splitSt[20]);
 //                    Double d_km = Double.valueOf(splitSt[19]);
 //                    BigDecimal link_length_km = BigDecimal.valueOf(d_km);
 //                    Double d_m = Double.valueOf(splitSt[20]);
 //                    BigDecimal link_length_miles = BigDecimal.valueOf(d_m);
-                   // String road_name = splitSt[9];
-                    
-                   
-                    String sqlString = "INSERT INTO Count_Point (count_point_id, start_junction_road_name, end_junction_road_name, easting, northing, latitude, longitude, link_length_km, link_length_miles)"
-                                                        + "VALUES ('" + count_point_id + "','" + start_junction_road_name + "','" + end_junction_road_name + "','" + easting + "','" + northing + "','" + latitude + "','" + longitude + "','" + link_length_km + "','" + link_length_miles + "');";
-                   
+                    String road_name = splitSt[9];
+
+                    String sqlString = "INSERT INTO Count_Point (count_point_id, start_junction_road_name, end_junction_road_name, easting, northing, latitude, longitude, link_length_km, link_length_miles, road_name)"
+                            + "VALUES ('" + count_point_id + "','" + start_junction_road_name + "','" + end_junction_road_name + "','" + easting + "','" + northing + "','" + latitude + "','" + longitude + "','" + link_length_km + "','" + link_length_miles + "','" + road_name + "');";
+
                     System.out.println("sqlString : " + sqlString);
-                    
+
                     con.setAutoCommit(false);
                     stmt = con.createStatement();
 
@@ -154,8 +150,7 @@ else {
                 } else {
                     System.out.println("Duplicate record on count with pk :" + splitSt[0]);
                 }
-                
-                
+
 //                if (!IdChecker.contains(splitSt[9])) {
 //                    IdChecker.add(splitSt[9]);
 //                    String road_name = splitSt[9];
@@ -176,11 +171,10 @@ else {
 //                    System.out.println("Duplicate record with pk :" + splitSt[9]);
 //                }
 //                
-               
             }
-        } catch (Exception e) {
+        } catch (IOException | NumberFormatException | SQLException e) {
             //System.out.println(e.getMessage());
-                        System.out.println("Error reading to database" );
+            System.out.println("Error reading to database");
 
         }
     }
